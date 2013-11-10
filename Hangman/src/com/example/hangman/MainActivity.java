e3b0e1384408a867928807418206be7d83cb3040
Jos Bonsink <josbonsink@gmail.com>
2013-11-10 20:41:08 +0100

    Virtual keyboard

--- Hangman/src/com/example/hangman/MainActivity.java
+++ Hangman/src/com/example/hangman/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.hangman;
 
+import java.util.HashMap;
+
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import com.example.hangman.VirtualKeyboard;;
 
 public class MainActivity extends Activity 
 {
-
+	private VirtualKeyboard keyboard;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) 
-    {
+    {   				
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);       
+        setContentView(R.layout.activity_main);        
+    }
+    
+    @Override
+    protected void onStart()
+    {
+    	super.onStart();
+    	
+    	Resources resources = getResources();
+    	Drawable[] drawables = new Drawable[3];
+    	drawables[0] = resources.getDrawable(R.drawable.correct_guess);
+    	drawables[1] = resources.getDrawable(R.drawable.incorrect_guess);
+    	drawables[2] = resources.getDrawable(android.R.drawable.bottom_bar);
+    	
+    	keyboard = new VirtualKeyboard(GetKeyboardButtons(), drawables);    	
+    }
+        
+    private HashMap<Character, Button> GetKeyboardButtons()
+    {
+    	HashMap<Character, Button> buttons = new HashMap<Character, Button>();
+    	for (char letter = 'A'; letter <= 'Z'; letter++)
+		{    		    		
+			int resourceID = getResources().getIdentifier("Key" + letter, "id", getPackageName());			
+			Button button = (Button) findViewById(resourceID);			
+			buttons.put(letter, button);			
+		}    	
+    	return buttons;
     }
 }
--- Hangman/src/com/example/hangman/VirtualKeyboard.java
+++ Hangman/src/com/example/hangman/VirtualKeyboard.java
@@ -1,17 +1,59 @@
 package com.example.hangman;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import android.graphics.drawable.Drawable;
+import android.view.View;
 import android.widget.Button;
 
 public class VirtualKeyboard
-{
+{	
 	private Map<Character, Button> buttons;
-
-	public VirtualKeyboard()
+	private Drawable correct;
+	private Drawable incorrect;
+	private Drawable normal;
+	
+	public VirtualKeyboard(Map<Character, Button> buttons, Drawable[] buttonStates)
 	{
-		buttons = new HashMap<Character, Button>();
+		this.correct = buttonStates[0];
+		this.incorrect = buttonStates[1];
+		this.normal = buttonStates[2];
+		this.buttons = buttons;
 		
+		for (Button button : buttons.values())
+		{
+			button.setOnClickListener(new View.OnClickListener() 
+			{				
+				@Override
+				public void onClick(View view) 
+				{					 
+					onTouch(view);
+				}
+			});
+		}
+	}
+	
+	private void onTouch(View view)
+	{
+		Button button = (Button)view;
+		button.setBackgroundDrawable(correct);
+		button.setEnabled(false);
+	}
+	
+	private void Highlight(char letter)
+	{
+		Button button = buttons.get(letter);
+		
+		//button.setBackgroundDrawable(correct);
+		//button.setBackgroundDrawable(incorrect);
+	}
+	
+	private void Reset()
+	{
+		for (Button button : buttons.values())
+		{
+			button.setBackgroundDrawable(normal);
+			button.setEnabled(true);
+		}
 	}
 }
